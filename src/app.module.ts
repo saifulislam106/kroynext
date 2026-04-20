5e348637cd3f6c9192510e54e41d3c824625598d
saifulislam106 <saifulislam106915@gmail.com>
2026-04-20 23:59:17 +0600

    order: commited

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,9 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
 import { CategoryModule } from './modules/category/category.module';
 import { ProductModule } from './modules/product/product.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -14,12 +17,24 @@ import { ProductModule } from './modules/product/product.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
+      ThrottlerModule.forRoot([
+      {
+        ttl: 60, // seconds
+        limit: 10, // 10 requests per 60 seconds
+      },
+    ]),
     PrismaModule,
      AuthModule,
      UserModule,
      CategoryModule,
-     ProductModule],
-  controllers: [AppController],
-  providers: [AppService],
+     ProductModule,
+     OrdersModule],
+    controllers: [AppController],
+    providers: [AppService,
+      {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
